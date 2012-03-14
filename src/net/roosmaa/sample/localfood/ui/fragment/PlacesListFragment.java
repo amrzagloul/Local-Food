00a4afde66c3a049425ce19e8005efc21b1ed24c
Mart Roosmaa <roosmaa@gmail.com>
2012-03-14 13:35:14 +0000

    Added CursorLoader to fragments.

--- src/net/roosmaa/sample/localfood/ui/fragment/PlacesListFragment.java
+++ src/net/roosmaa/sample/localfood/ui/fragment/PlacesListFragment.java
@@ -1,8 +1,74 @@
 package net.roosmaa.sample.localfood.ui.fragment;
 
+import net.roosmaa.sample.localfood.R;
+import net.roosmaa.sample.localfood.provider.FoodContract.Restaurants;
+import android.database.Cursor;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
 
-public class PlacesListFragment extends ListFragment
+public class PlacesListFragment extends ListFragment implements
+    LoaderManager.LoaderCallbacks<Cursor>
 {
+  private SimpleCursorAdapter mAdapter;
   
+  private static final String[] CURSOR_PROJECTION = new String[] {
+      Restaurants.PLACE_ID,
+      Restaurants.PLACE_NAME,
+      Restaurants.PLACE_RATING,
+      Restaurants.PLACE_VICINITY,
+  };
+  
+  private static final String[] ADAPTER_MAP_FROM = new String[] {
+      Restaurants.PLACE_NAME,
+      Restaurants.PLACE_RATING,
+      Restaurants.PLACE_VICINITY,
+  };
+  
+  private static final int[] ADAPTER_MAP_TO = new int[] {
+      R.id.text_name,
+      R.id.text_score,
+      R.id.text_address,
+  };
+  
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState)
+  {
+    super.onActivityCreated(savedInstanceState);
+    
+    mAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item_place,
+        null, ADAPTER_MAP_FROM, ADAPTER_MAP_TO, 0);
+    setListAdapter(mAdapter);
+    
+    setListShown(false);
+    
+    getLoaderManager().initLoader(0, null, this);
+  }
+  
+  @Override
+  public Loader<Cursor> onCreateLoader(int id, Bundle args)
+  {
+    return new CursorLoader(getActivity(), Restaurants.CONTENT_URI,
+        CURSOR_PROJECTION, null, null, Restaurants.DEFAULT_SORT);
+  }
+  
+  @Override
+  public void onLoadFinished(Loader<Cursor> loader, Cursor data)
+  {
+    mAdapter.swapCursor(data);
+    
+    if (isResumed())
+      setListShown(true);
+    else
+      setListShownNoAnimation(true);
+  }
+  
+  @Override
+  public void onLoaderReset(Loader<Cursor> loader)
+  {
+    mAdapter.swapCursor(null);
+  }
 }
--- src/net/roosmaa/sample/localfood/ui/fragment/PlacesMapFragment.java
+++ src/net/roosmaa/sample/localfood/ui/fragment/PlacesMapFragment.java
@@ -1,8 +1,49 @@
 package net.roosmaa.sample.localfood.ui.fragment;
 
+import net.roosmaa.sample.localfood.provider.FoodContract.Restaurants;
+import android.database.Cursor;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 
-public class PlacesMapFragment extends Fragment
+public class PlacesMapFragment extends Fragment implements
+    LoaderManager.LoaderCallbacks<Cursor>
 {
+  private Cursor mCursor;
   
+  private static final String[] CURSOR_PROJECTION = new String[] {
+      Restaurants.PLACE_ID,
+      Restaurants.PLACE_LAT,
+      Restaurants.PLACE_LNG,
+      Restaurants.PLACE_NAME,
+  };
+  
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState)
+  {
+    super.onActivityCreated(savedInstanceState);
+    
+    getLoaderManager().initLoader(0, null, this);
+  }
+  
+  @Override
+  public Loader<Cursor> onCreateLoader(int id, Bundle args)
+  {
+    return new CursorLoader(getActivity(), Restaurants.CONTENT_URI,
+        CURSOR_PROJECTION, null, null, null);
+  }
+  
+  @Override
+  public void onLoadFinished(Loader<Cursor> loader, Cursor data)
+  {
+    mCursor = data;
+  }
+  
+  @Override
+  public void onLoaderReset(Loader<Cursor> loader)
+  {
+    mCursor = null;
+  }
 }
